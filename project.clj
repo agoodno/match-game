bf0621155420f0dd9d6dfb0c3eda48bdb6abde3e
Andrew Goodnough <agoodno@gmail.com>
2013-02-19 23:05:42 -0600

    Heroku config

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
                  [enlive "1.0.0"]
                  [noir "1.3.0-beta3"]
                  [jayq "2.0.0"]]
+  :min-lein-version "2.0.0"
   :source-paths ["src/main/clojure"]
 ;;  :resource-paths ["src/main/resource"]
   :test-paths ["src/test/clojure"]
