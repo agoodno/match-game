ba0fd5968b9370fe019de987a176b35fbea385f0
Andrew Goodnough <agoodno@gmail.com>
2013-03-03 22:56:47 -0600

    Added a new 'oldmaid' cardset
    * Hard-coded support for tall images
    * Changed the card cover image
    * Hover is now implemented in CSS so hover images are no longer needed
    * Simplified remaining CSS

--- src/main/clojure/match_game/views/game.clj
+++ src/main/clojure/match_game/views/game.clj
@@ -6,54 +6,57 @@
         clojure.pprint))
 
 (defn css-line [card]
-  (let [card-selector (str "#" (:id card))]
+  (let [card-selector (str "." (:id card))]
     (str
-     (str card-selector " { background-image: url('" (:normal-path card) "'); }\n")
-     (str card-selector ".hovering { background-image: url('" (:hover-path card) "'); }\n")
-     (str card-selector ".covered { background-image: url('/img/cover.png'); }\n")
-     (str card-selector ".covered.hovering { background-image: url('/img/cover_hover.png'); }\n"))))
+     (str card-selector " { background-image: url('" (:normal-path card) "'); }"))))
 
 (defn css-lines [cards]
   (for [card cards] (css-line card)))
 
+;call (card-base-name k cards)
 (defn card-base-name [card-id cards]
   (let [{{base-name :base-name} card-id} cards]
     base-name))
 
 (html/defsnippet game-snippet
   "match_game/views/game.html" [:html]
-  [cards]
+  [unique-cards game-cards]
   
   [:#boardCss]
-  (html/content (clojure.string/join "\n" (css-lines cards)))
+  (html/content (clojure.string/join "\n" (css-lines unique-cards)))
 
   [:img.card]
-  (html/clone-for [card cards]
-                  (html/set-attr :id (:id card)))) ;; :src v   [[{:base-name}] v]  :data-base-name (card-base-name k cards)
+  (html/clone-for [card game-cards]
+                  (html/do->
+                      (html/set-attr :id (:id card))
+                      (html/add-class (:base-name card)))))
 
 (defn get-card [name cardset name-ext]
   (let
       [file-path (fn [n] (format "/img/cardsets/%s/%s.png" cardset n))
-       normal-path (file-path name)
-       hover-path (file-path (str "hover/" name))]
+       normal-path (file-path name)]
     {:id (str name name-ext)
      :base-name name
-     :normal-path normal-path
-     :hover-path hover-path}))
+     :normal-path normal-path}))
 
 (defn short-path [filePath] (last (re-find #"^(.*)(/img/.*)$" filePath)))
 
-(defn cardset [cardset-name]
+(defn ids-in-cardset [cardset-name]
   (let [cardset-dirpath
         (str (.getFile (clojure.java.io/resource "public/img/cardsets/")) cardset-name)
         files (core/list-dir cardset-dirpath #".*\.png")
         file-strs (map #(.getPath %) files)
-        short-paths (map #(short-path %) file-strs)
-        ids (map #(last (re-find #"^.*\/(.*)\.png$" %)) short-paths)]
+        short-paths (map #(short-path %) file-strs)]
+        (map #(last (re-find #"^.*\/(.*)\.png$" %)) short-paths)))
+
+(defn cards-in-cardset [cardset-name]
+    (map #(get-card %1 cardset-name "") (ids-in-cardset cardset-name)))
+
+(defn cards-in-round [cardset-name]
+  (let [ids (ids-in-cardset cardset-name)]
     (core/randomize
      (concat (map #(get-card %1 cardset-name "-left") ids)
              (map #(get-card %1 cardset-name "-right") ids)))))
-
 
 ;; Save for client js code
 ;; (defn get-cards [cardset]
@@ -65,5 +68,4 @@
 ;;       cards)))
 
 (defpage "/game" {:keys [card-style]}
-  (let [cards (cardset card-style)]
-    (html/emit* (game-snippet cards))))
+    (html/emit* (game-snippet (cards-in-cardset card-style) (cards-in-round card-style))))
