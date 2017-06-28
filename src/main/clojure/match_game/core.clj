e164b1b63982e31ab0db449c9251bf65270e3b21
Andrew Goodnough <agoodnough@primexinc.com>
2017-06-28 08:59:30 -0500

    Renames domains and impls more in flip state
    * Deck become theme
    * Card became design
    * Slot became card
    * Goes back to merging design into card on board
    * Flips state name as well as img

--- src/main/clojure/match_game/core.clj
+++ src/main/clojure/match_game/core.clj
@@ -1,7 +1,8 @@
 (ns match-game.core
   (:require [clojure.java.io :refer [file]]
             [clojure.string :as str]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [clojure.set :refer [rename-keys]]))
 
 (defn walk-dir [dirpath pattern]
   (doall (filter #(re-matches pattern (.getName %))
@@ -17,16 +18,16 @@
          (sort-by first
                   (map list indexes coll)))))
 
-(defn files-in-deck [deck-name]
-  (let [deck-dirpath (str (.getFile (clojure.java.io/resource "public/img/decks/")) deck-name)
-        files (list-dir deck-dirpath #".*\.png")]
+(defn files-in-theme [theme-name]
+  (let [theme-dirpath (str (.getFile (clojure.java.io/resource "public/img/themes/")) theme-name)
+        files (list-dir theme-dirpath #".*\.png")]
     (map #(.getPath %) files)))
 
 (defn short-path [filePath]
   (last (re-find #"^(.*)(/img/.*)$" filePath)))
 
-(defn names-in-deck [deck-name]
-  (let [file-strs (files-in-deck deck-name)
+(defn names-in-theme [theme-name]
+  (let [file-strs (files-in-theme theme-name)
         short-paths (map #(short-path %) file-strs)]
     (map #(last (re-find #"^.*\/(.*)\.png$" %)) short-paths)))
 
@@ -39,67 +40,72 @@
 (defn uri-path [dir base-name]
   (str "/" dir "/" base-name ".png"))
 
-(defn load-card [deck-name card-name]
-  {:name card-name
-   :description (human-name card-name)
-   :img (uri-path deck-name card-name)})
+(defn load-design [theme-name design-name]
+  {:name design-name
+   :description (human-name design-name)
+   :img (uri-path theme-name design-name)})
 
-;;     :cards (group-by :name (map load-card deck-names card-names))
-(defn load-deck [deck-name]
-  (let [card-names (names-in-deck deck-name)
-        deck-names (repeat (count card-names) deck-name)
-        card-map (group-by :name (map load-card deck-names card-names))]
-    {:name deck-name
-     :description (human-name deck-name)
-     :cards (zipmap (map keyword (keys card-map)) (map #(first %) (vals card-map)))}))
+(defn load-theme [theme-name]
+  (let [design-names (names-in-theme theme-name)
+        theme-names (repeat (count design-names) theme-name)]
+    {:name theme-name
+     :description (human-name theme-name)
+     :designs (map load-design theme-names design-names)}))
 
-(def get-deck (memoize load-deck))
+(def get-theme (memoize load-theme))
 
 (def players
   {:agoodno { :name "Andy" }
    :cjgoodno { :name "Cal" }})
 
-(defn init-slot [card twin]
-  {:twin twin :state :covered :card-key (keyword (:name card))})
+(defn init-card [face-design twin]
+  (let [back-design { :back-img "/hoyle.png" }]
+    (->
+     (merge {:twin twin :state {:name :covered :img (:back-img back-design)}} face-design)
+     (merge ,,, back-design)
+     (rename-keys ,,, {:img :face-img}))))
 
-(defn init-slots [cards]
+(defn init-cards [designs]
   (let [twins [:left :right]]
     (shuffle
      (mapcat (fn [twin]
-               (map (fn [card]
-                      (init-slot card twin))
-                    cards))
+               (map (fn [design]
+                      (init-card design twin))
+                    designs))
              twins))))
 
-(defn init-board [dimensions background-color cards]
+(defn init-board [dimensions background-color designs]
   {:dimensions dimensions
    :background-color background-color
-   :slots (init-slots cards)})
+   :cards (init-cards designs)})
 
-(defn init-game [deck-name]
+(defn init-game [theme-name]
   (let [dimensions [4 5]
         background-color "black"
-        deck (get-deck deck-name)
-        cards (vals (:cards deck))]
+        theme (get-theme theme-name)
+        designs (:designs theme)]
     {:players players
      :turn :cjgoodno
-     :deck deck
-     :board (init-board dimensions background-color cards)}))
+     :theme theme
+     :board (init-board dimensions background-color designs)}))
 
-(defn flip [state] (if (= :covered state) :facing :covered))
+(defn flip-state [state] (if (= :covered state) :facing :covered))
 
-(defn flip-slot [game idx]
-  (let [board (:board @game)
-        slots (:slots board)
-        slot (get slots idx)]
-    (assoc @game :board
-           (assoc board :slots
-                  (assoc slots idx (update slot :state flip))))))
+(defn flip-img [img card]
+  (if (= "/hoyle.png" img) (:face-img card) (:back-img card)))
+
+(defn flip-card [game idx]
+  (let [board (:board game)
+        cards (:cards board)
+        card (get cards idx)]
+    (->
+     (update-in game [:board :cards idx :state :name] flip-state)
+     (update-in ,,, [:board :cards idx :state :img] flip-img card))))
 
 (defn -main
   [& args]
-  (let [[deck-name] args
-        game (atom (init-game deck-name))]
+  (let [[theme-name] args
+        game (atom (init-game theme-name))]
     (pp/pprint game)))
 
-;;(reset! game (match-game.core/flip-slot game 2))
+;;(reset! game (match-game.core/flip-card game 2))
